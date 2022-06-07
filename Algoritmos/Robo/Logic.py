2f0fedfafd8a29f88ae105127b54118961098184
MiguelANunes <miguel.alfredo.nunes@gmail.com>
2022-06-07 15:15:52 -0300

    Pequeno progresso robo

--- Algoritmos/Robo/Logic.py
+++ Algoritmos/Robo/Logic.py
@@ -8,14 +8,14 @@ class Item:
     # 3: Dispositivo de refrigeração;
     # 4: Braço pneumático.
     def __init__(self, tipo, pos):
-        self.tipo = tipo
-        self.pos = pos
+        self.tipo     = tipo
+        self.position = pos
     
         if self.tipo == 0:
             self.cor = (32,32,32)
 
         elif self.tipo == 1:
-            self.cor = (164,0,0)
+            self.cor = (200,0,0)
 
         elif self.tipo == 2:
             self.cor = (240,0,200)
@@ -83,7 +83,7 @@ class Fabrica:
 
     def __init__(self, tipo, pos, qtd=None, obj=None):
         self.tipo    = tipo
-        self.pos     = pos
+        self.position     = pos
         self.request = (qtd, obj) # request será uma tupla (int, int), segundo int é o tipo do item
 
         if self.tipo == 0:
@@ -112,9 +112,11 @@ class Robo:
     # contents => o que o robô está carregando, lista de ints, cada int é um tipo de item
     # lookingAt => célula que o robô está olhando enquanto executa o A*, usado para representar a decisão dele
     def __init__(self, pos, target=None, contents=None, lookingAt=None):
-        self.pos       = pos
+        self.position  = pos
+        self.pastPos   = (-1,-1)
         self.target    = target if target != None else (-1,-1)
         self.lookingAt = lookingAt if lookingAt != None else (-1,-1)
+        self.path      = []
         self.contents  = contents if contents != None else []
         self.radius    = 4
 
@@ -128,107 +130,122 @@ class Robo:
             cell.remove()
             return True
 
+    def move(self, newPos):
+        self.pastPos  = self.position
+        self.position = newPos
+
+    def follow_path(self):
+        self.move(self.path.pop(0))
+
 def load_map():
-    simMap = []
+    simulationMap = []
     with open(f"inputs/input.txt") as f:
         for line in f:
             line = (line.replace("\t"," ")).replace(",",".")
             cells = [l.rstrip('\n') for l in line.split(" ") if l != '' and l != '\n']
             cell = [Celula(int(c)) for c in cells]
-            simMap.append(cell)
-    return simMap
+            simulationMap.append(cell)
+    return simulationMap
 
-def generate_factories(simMap):
+def generate_factories(simulationMap):
 
     factoryList = []
 
     pos = (randint(0, 41), randint(0, 41))
-    while simMap[pos[0]][pos[1]].contents != None:
+    while simulationMap[pos[0]][pos[1]].contents != None:
         pos = (randint(0, 41), randint(0, 41))
     fabricaGraos = Fabrica(0, pos, 8, 0)
     fabricaGraos.set_request(8, 0)
-    simMap[pos[0]][pos[1]].place(fabricaGraos)
+    simulationMap[pos[0]][pos[1]].place(fabricaGraos)
     factoryList.append(fabricaGraos)
 
     pos = (randint(0, 41), randint(0, 41))
-    while simMap[pos[0]][pos[1]].contents != None:
+    while simulationMap[pos[0]][pos[1]].contents != None:
         pos = (randint(0, 41), randint(0, 41))
     fabricaBarcos = Fabrica(1, pos, 5, 1)
     fabricaBarcos.set_request(5, 1)
-    simMap[pos[0]][pos[1]].place(fabricaBarcos)
+    simulationMap[pos[0]][pos[1]].place(fabricaBarcos)
     factoryList.append(fabricaBarcos)
 
     pos = (randint(0, 41), randint(0, 41))
-    while simMap[pos[0]][pos[1]].contents != None:
+    while simulationMap[pos[0]][pos[1]].contents != None:
         pos = (randint(0, 41), randint(0, 41))
     fabricaPetrobras = Fabrica(2, pos, 2, 2)
     fabricaPetrobras.set_request(2, 2)
-    simMap[pos[0]][pos[1]].place(fabricaPetrobras)
+    simulationMap[pos[0]][pos[1]].place(fabricaPetrobras)
     factoryList.append(fabricaPetrobras)
 
     pos = (randint(0, 41), randint(0, 41))
-    while simMap[pos[0]][pos[1]].contents != None:
+    while simulationMap[pos[0]][pos[1]].contents != None:
         pos = (randint(0, 41), randint(0, 41))
     fabricaFundicao = Fabrica(3, pos, 5, 3)
     fabricaFundicao.set_request(5, 3)
-    simMap[pos[0]][pos[1]].place(fabricaFundicao)
+    simulationMap[pos[0]][pos[1]].place(fabricaFundicao)
     factoryList.append(fabricaFundicao)
 
     pos = (randint(0, 41), randint(0, 41))
-    while simMap[pos[0]][pos[1]].contents != None:
+    while simulationMap[pos[0]][pos[1]].contents != None:
         pos = (randint(0, 41), randint(0, 41))
     fabricaVigas = Fabrica(4, pos, 2, 4)
     fabricaVigas.set_request(2, 4)
-    simMap[pos[0]][pos[1]].place(fabricaVigas)
+    simulationMap[pos[0]][pos[1]].place(fabricaVigas)
     factoryList.append(fabricaVigas)
 
     return factoryList
 
-def generate_items(simMap):
+def generate_items(simulationMap):
 
     itemList = []
 
     for _ in range(20):
         pos = (randint(0, 41), randint(0, 41))
-        while simMap[pos[0]][pos[1]].tipo != 0 and simMap[pos[0]][pos[1]].contents != None:
+        while simulationMap[pos[0]][pos[1]].tipo != 0 and simulationMap[pos[0]][pos[1]].contents != None:
             pos = (randint(0, 41), randint(0, 41))
         itemBateria = Item(0, pos)
-        simMap[pos[0]][pos[1]].place(itemBateria)
+        simulationMap[pos[0]][pos[1]].place(itemBateria)
         itemList.append(itemBateria)
 
     for _ in range(10):
         pos = (randint(0, 41), randint(0, 41))
-        while simMap[pos[0]][pos[1]].tipo != 0 and simMap[pos[0]][pos[1]].contents != None:
+        while simulationMap[pos[0]][pos[1]].tipo != 0 and simulationMap[pos[0]][pos[1]].contents != None:
             pos = (randint(0, 41), randint(0, 41))
         itemBraco = Item(1, pos)
-        simMap[pos[0]][pos[1]].place(itemBraco)
+        simulationMap[pos[0]][pos[1]].place(itemBraco)
         itemList.append(itemBraco)
     
     for _ in range(8):
         pos = (randint(0, 41), randint(0, 41))
-        while simMap[pos[0]][pos[1]].tipo != 0 and simMap[pos[0]][pos[1]].contents != None:
+        while simulationMap[pos[0]][pos[1]].tipo != 0 and simulationMap[pos[0]][pos[1]].contents != None:
             pos = (randint(0, 41), randint(0, 41))
         itemBomba = Item(2, pos)
-        simMap[pos[0]][pos[1]].place(itemBomba)
+        simulationMap[pos[0]][pos[1]].place(itemBomba)
         itemList.append(itemBomba)
 
     for _ in range(6):
         pos = (randint(0, 41), randint(0, 41))
-        while simMap[pos[0]][pos[1]].tipo != 0 and simMap[pos[0]][pos[1]].contents != None:
+        while simulationMap[pos[0]][pos[1]].tipo != 0 and simulationMap[pos[0]][pos[1]].contents != None:
             pos = (randint(0, 41), randint(0, 41))
         itemRefrigeracao = Item(3, pos)
-        simMap[pos[0]][pos[1]].place(itemRefrigeracao)
+        simulationMap[pos[0]][pos[1]].place(itemRefrigeracao)
         itemList.append(itemRefrigeracao)
 
     for _ in range(4):
         pos = (randint(0, 41), randint(0, 41))
-        while simMap[pos[0]][pos[1]].tipo != 0 and simMap[pos[0]][pos[1]].contents != None:
+        while simulationMap[pos[0]][pos[1]].tipo != 0 and simulationMap[pos[0]][pos[1]].contents != None:
             pos = (randint(0, 41), randint(0, 41))
         itemBracoPneumatico = Item(4, pos)
-        simMap[pos[0]][pos[1]].place(itemBracoPneumatico)
+        simulationMap[pos[0]][pos[1]].place(itemBracoPneumatico)
         itemList.append(itemBracoPneumatico)
 
     return itemList
 
 def is_valid(pos):
     return pos[0] >= 0 and pos[1] >= 0 and pos[0] < 42 and pos[1] < 42
+
+def move_robot(robot, simulationMap, listItems, listFactories):
+    pass
+    # procura por itens que ainda não tem
+    # move aleatóriamente até achar algo
+    # tendo algo, continua movendo aleatório
+    # se achar uma fábrica que precisa de algo e tem, entrega
+    # se todas as fábricas estiverem satisfeitas, termina simulação
--- Algoritmos/Robo/Render.py
+++ Algoritmos/Robo/Render.py
@@ -3,40 +3,46 @@ from pygame.locals import *
 
 import Logic
 
-def draw(simMap, itemList, factoryList, robot, DISPLAY, width, height):
+def draw(simulationMap, itemList, factoryList, robot, DISPLAY, width, height):
     pos_x = 0 # linha onde vai desenhar
     for x in range(42):
         pos_y = 0 # coluna onde vai desenhar
         for y in range(42):
-            Cor = simMap[y][x].cor
+            Cor = simulationMap[y][x].cor
             # desenhando as células do mapa
             pygame.draw.rect(DISPLAY, Cor, (18*pos_x, 16*pos_y, 18, 16))
             pos_y += 1
         pos_x += 1
 
     for item in itemList: # desenhando os itens
-        pygame.draw.circle(DISPLAY, item.cor, ((18*item.pos[0])+9, (16*item.pos[1])+8), 5)
+        pygame.draw.circle(DISPLAY, item.cor, ((18*item.position[0])+9, (16*item.position[1])+8), 5)
 
     for factory in factoryList: # desenhando as fábricas
-        pygame.draw.rect(DISPLAY, factory.cor, ((18*factory.pos[0])+3, (16*factory.pos[1])+2, 14, 12))
+        pygame.draw.rect(DISPLAY, factory.cor, ((18*factory.position[0])+3, (16*factory.position[1])+2, 14, 12))
 
     # desenhando o robô
-    pygame.draw.rect(DISPLAY, (255,255,255), ((18*robot.pos[0])+3, (16*robot.pos[1])+2, 14, 12))
-    pygame.draw.rect(DISPLAY, (0,0,0), ((18*robot.pos[0])+3, (16*robot.pos[1])+2, 14, 12), 2)
+    pygame.draw.rect(DISPLAY, (255,255,255), ((18*robot.position[0])+3, (16*robot.position[1])+2, 14, 12))
+    pygame.draw.rect(DISPLAY, (0,0,0), ((18*robot.position[0])+3, (16*robot.position[1])+2, 14, 12), 2)
         
     for x in range(0,756,18): # desenhando linhas verticais separadoras
         pygame.draw.line(DISPLAY, (64,64,64), (x, 0), (x, height))
     for y in range(0,672,16): # desenhando linhas horizontais separadoras
         pygame.draw.line(DISPLAY, (64,64,64), (0, y), (width, y))
 
-    if Logic.is_valid(robot.target): # se o robô quer chegar em alguma célula
+    if Logic.is_valid(robot.target): # marcando célula alvo do movimento do robô
         pygame.draw.line(DISPLAY, (255,0,0), ((18*robot.target[0])-18, (16*robot.target[1])-16), ((18*robot.target[0]),    (16*robot.target[1])-16))
         pygame.draw.line(DISPLAY, (255,0,0), ((18*robot.target[0])-18, (16*robot.target[1])-16), ((18*robot.target[0])-18, (16*robot.target[1])))
         pygame.draw.line(DISPLAY, (255,0,0), ((18*robot.target[0])-18, (16*robot.target[1])),    ((18*robot.target[0]),    (16*robot.target[1])))
         pygame.draw.line(DISPLAY, (255,0,0), ((18*robot.target[0]),    (16*robot.target[1])-16), ((18*robot.target[0]),    (16*robot.target[1])))
 
-    if Logic.is_valid(robot.lookingAt): # se o robô está olhando pra alguma célula
+    if Logic.is_valid(robot.lookingAt): # marcando células que o robô está analisando para mover
         pygame.draw.line(DISPLAY, (255,255,255), ((18*robot.lookingAt[0])-18, (16*robot.lookingAt[1])-16), ((18*robot.lookingAt[0]),    (16*robot.lookingAt[1])-16))
         pygame.draw.line(DISPLAY, (255,255,255), ((18*robot.lookingAt[0])-18, (16*robot.lookingAt[1])-16), ((18*robot.lookingAt[0])-18, (16*robot.lookingAt[1])))
         pygame.draw.line(DISPLAY, (255,255,255), ((18*robot.lookingAt[0])-18, (16*robot.lookingAt[1])),    ((18*robot.lookingAt[0]),    (16*robot.lookingAt[1])))
         pygame.draw.line(DISPLAY, (255,255,255), ((18*robot.lookingAt[0]),    (16*robot.lookingAt[1])-16), ((18*robot.lookingAt[0]),    (16*robot.lookingAt[1])))
+
+    for cell in robot.path: # marcando as células no path selecionado pelo robô
+        pygame.draw.line(DISPLAY, (255,255,0), ((18*cell[0])-18, (16*cell[1])-16), ((18*cell[0]),    (16*cell[1])-16))
+        pygame.draw.line(DISPLAY, (255,255,0), ((18*cell[0])-18, (16*cell[1])-16), ((18*cell[0])-18, (16*cell[1])))
+        pygame.draw.line(DISPLAY, (255,255,0), ((18*cell[0])-18, (16*cell[1])),    ((18*cell[0]),    (16*cell[1])))
+        pygame.draw.line(DISPLAY, (255,255,0), ((18*cell[0]),    (16*cell[1])-16), ((18*cell[0]),    (16*cell[1])))
